70992580adcc0c8e8f43abc7d4beb5f9ef82da75
Renzheng Zhang <renzheng@renzheng.me>
2023-04-13 21:40:09 +0800

    init repo.

---
+++ src/test/java/me/renzheng/spring/security/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.renzheng.spring.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
